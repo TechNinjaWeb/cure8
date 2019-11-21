3cfd15b7a764e9b0604f6c2c906d697dca6c4c60
Bulatbik <bulatmegavolt@gmail.com>
2019-11-21 15:24:07 -0500

    save

---
+++ src/graphql/mutations.js
@@ -0,0 +1,27 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const createBook = `mutation CreateBook($input: CreateBookInput!) {
+  createBook(input: $input) {
+    id
+    title
+    author
+  }
+}
+`;
+export const updateBook = `mutation UpdateBook($input: UpdateBookInput!) {
+  updateBook(input: $input) {
+    id
+    title
+    author
+  }
+}
+`;
+export const deleteBook = `mutation DeleteBook($input: DeleteBookInput!) {
+  deleteBook(input: $input) {
+    id
+    title
+    author
+  }
+}
+`;
